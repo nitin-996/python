c41bab98a615bd18d27fd955619cc2088c8dfa56
Nitin <sachinles99@gmail.com>
2025-07-18 18:55:36 +0530

    practice

--- useful libraries/03 files.py
+++ useful libraries/03 files.py
@@ -1,5 +1,13 @@
 from pathlib import Path
+from time import ctime
+path = Path("python/01_variable.py")
+path.exists()
+# path.rename("new variable")
+# path.unlink()
 
-path = Path()
+print(ctime(path.stat().st_ctime))
 
+path.write_bytes()
+path.read_bytes()
 print(path)
+
